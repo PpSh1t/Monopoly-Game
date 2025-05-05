e2811bbcdcd7f709f3492cd1c679fdb2c6d108ea
PpSh1t <131457643+PpSh1t@users.noreply.github.com>
2025-05-05 22:52:10 +0800

    feature:开发骰子功能

--- src/main/java/logic/Dice.java
+++ src/main/java/logic/Dice.java
@@ -1,1 +1,13 @@
 package logic;
+
+import java.util.Random;
+
+public class Dice {
+    private static final Random rand = new Random();
+
+    public static int roll() {
+        int d1 = rand.nextInt(6) + 1;
+        int d2 = rand.nextInt(6) + 1;
+        return d1 + d2;
+    }
+}
