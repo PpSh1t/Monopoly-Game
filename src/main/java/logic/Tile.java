6c7d42d3149c60fb4e0c72ad1beb175bb1d137a8
PpSh1t <131457643+PpSh1t@users.noreply.github.com>
2025-05-05 23:10:38 +0800

    feature:地块的操作逻辑框架搭建

--- src/main/java/logic/Tile.java
+++ src/main/java/logic/Tile.java
@@ -2,7 +2,7 @@ package logic;
 
 public class Tile {
     public enum TileType {
-        START, LAND, LUCKY, HOSPITAL, PRISON, TAX, UNLUCKY
+        START, LAND, LUCKY, UNLUCKY, PRISON, HOSPITAL, TAX
     }
 
     private TileType type;  // 地块类型
@@ -15,6 +15,38 @@ public class Tile {
         this.level = 0;  // 默认等级为 0
         this.owner = null;  // 默认没有拥有者
     }
+
+    public void handleTile(Player player, Tile tile) {
+        switch (tile.getType()) {
+            case LAND:
+                if (tile.getOwner() == null) {
+                    // 空地：提示购买
+                } else if (tile.getOwner().equals(player.getName())) {
+                    // 自己的地：允许升级
+                } else {
+                    // 别人的地：支付租金
+                }
+                break;
+            case LUCKY:
+                // 给玩家奖励
+                break;
+            case UNLUCKY:
+                // 扣钱或跳过一回合
+                break;
+            case PRISON:
+                // 设置玩家状态为“暂停一回合”
+                break;
+            case HOSPITAL:
+                // 扣医疗费 + 暂停一回合
+                break;
+            case TAX:
+                // 扣税
+                break;
+            default:
+                break;
+        }
+    }
+
 
     // Getter 和 Setter 方法
     public TileType getType() {
