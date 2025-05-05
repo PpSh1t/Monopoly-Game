a2c733244422f33a6c9c390eba9da9f4bb619c74
PpSh1t <131457643+PpSh1t@users.noreply.github.com>
2025-05-05 22:57:12 +0800

    docs:添加骰子方法文档

--- src/main/java/logic/Dice.java
+++ src/main/java/logic/Dice.java
@@ -5,6 +5,10 @@ import java.util.Random;
 public class Dice {
     private static final Random rand = new Random();
 
+    /**
+     * 生成两个随机数（1~6），并返回它们的总和。
+     * @return 返回两个随机数总和
+     */
     public static int roll() {
         int d1 = rand.nextInt(6) + 1;
         int d2 = rand.nextInt(6) + 1;
