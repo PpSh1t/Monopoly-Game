147407d758097421d5c01e52672eb2ddc4f76f64
PpSh1t <131457643+PpSh1t@users.noreply.github.com>
2025-05-05 23:34:33 +0800

    feature:开发空地购买和支付租金功能

--- src/main/java/logic/Tile.java
+++ src/main/java/logic/Tile.java
@@ -16,36 +16,6 @@ public class Tile {
         this.owner = null;  // 默认没有拥有者
     }
 
-    public void handleTile(Player player, Tile tile) {
-        switch (tile.getType()) {
-            case LAND:
-                if (tile.getOwner() == null) {
-                    // 空地：提示购买
-                } else if (tile.getOwner().equals(player.getName())) {
-                    // 自己的地：允许升级
-                } else {
-                    // 别人的地：支付租金
-                }
-                break;
-            case LUCKY:
-                // 给玩家奖励
-                break;
-            case UNLUCKY:
-                // 扣钱或跳过一回合
-                break;
-            case PRISON:
-                // 设置玩家状态为“暂停一回合”
-                break;
-            case HOSPITAL:
-                // 扣医疗费 + 暂停一回合
-                break;
-            case TAX:
-                // 扣税
-                break;
-            default:
-                break;
-        }
-    }
 
     /**
      * 获取购买价格
